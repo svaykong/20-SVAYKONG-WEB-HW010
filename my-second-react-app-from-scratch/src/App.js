6285f95b8379bd8fd4c3c42d08e20d9231449c7b
svaykong <kongsvay55@gmail.com>
2020-05-08 22:51:30 +0700

    Adding My Second React App From Scratch

---
+++ my-second-react-app-from-scratch/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Svay Kong</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
